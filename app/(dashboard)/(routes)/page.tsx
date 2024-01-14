bccfb2b4ee478a95dcf4241d0a78de878c0550bf
rick516 <k.murakami516@gmail.com>
2024-01-14 16:14:15 +0900

    fix account button is added at navbar

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <p className="text-3xl font-thin text-sky-700">
         Hello, I&apos;m CursorBot. yayyay
       </p>
-      <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
 }
--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -12,7 +12,7 @@ export const MobileSidebar = () => {
       <SheetTrigger className="md:hidden pr-4 hover:opaciry-75 transition">
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-white">
+      <SheetContent side="left" className="p-0">
         <Sidebar />
       </SheetContent>
     </Sheet>
