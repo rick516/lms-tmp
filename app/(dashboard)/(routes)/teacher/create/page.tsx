6df867108e8db66e7771316ecebb11269b76bb66
rick516 <k.murakami516@gmail.com>
2024-01-14 16:50:09 +0900

    add: course creation page is added

---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+
+const CreatePage = () => {
+  return ( 
+    <div>
+      create page
+    </div>
+   );
+}
+ 
+export default CreatePage;
