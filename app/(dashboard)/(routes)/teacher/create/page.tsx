d7b1104f51cc83baae999621934bd3d21f4ff1f4
rick516 <k.murakami516@gmail.com>
2024-01-14 17:25:12 +0900

    fix: form with zod resolver added

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -2,13 +2,81 @@
 
 import * as z from "zod";
 import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "title is required." })
+})
 
 const CreatePage = () => {
-  return ( 
-    <div>
-      create page
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    }
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">
+          Name your coureses
+        </h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your coureses?
+          Don&apos;t worry, you can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Course Title
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. Advanced Web Development"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default CreatePage;
