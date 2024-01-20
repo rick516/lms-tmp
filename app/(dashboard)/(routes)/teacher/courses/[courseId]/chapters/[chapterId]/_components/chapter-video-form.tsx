2407d912609eb4ca9bb3ae1464ac736b209f7615
rick516 <k.murakami516@gmail.com>
2024-01-20 14:31:25 +0900

    add: setting Biome as formatter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -1,45 +1,49 @@
 "use client";
-import * as z from "zod";
-import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
-import toast from "react-hot-toast";
-import { useState } from "react";
-import { Pencil, PlusCircle, Video } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import MuxPlayer from "@mux/mux-player-react";
 // import { cn } from "@/lib/utils";
 import { Chapter, MuxData } from "@prisma/client";
-import { FileUpload } from "@/components/file-upload";
-import MuxPlayer from "@mux/mux-player-react";
+import axios from "axios";
+import { Pencil, PlusCircle, Video } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
 
 interface ChapterVideoFormProps {
-	initialData: Chapter & { muxData?: MuxData | null};
+	initialData: Chapter & { muxData?: MuxData | null };
 	courseId: string;
-	chapterId: string
+	chapterId: string;
 }
 
 const formSchema = z.object({
-	videoUrl: z.string().min(1, { message: "a video url is required" })
+	videoUrl: z.string().min(1, { message: "a video url is required" }),
 });
 
 export const ChapterVideoForm = ({
-	initialData, courseId, chapterId
+	initialData,
+	courseId,
+	chapterId,
 }: ChapterVideoFormProps) => {
-	const router = useRouter()
+	const router = useRouter();
 	const [isEditing, setIsEditing] = useState(false);
-
 
 	const toggleEdit = () => setIsEditing((current) => !current);
 
 	const form = useForm({
 		resolver: zodResolver(formSchema),
-		defaultValues: { videoUrl: initialData?.videoUrl || "" }
+		defaultValues: { videoUrl: initialData?.videoUrl || "" },
 	});
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+			await axios.patch(
+				`/api/courses/${courseId}/chapters/${chapterId}`,
+				values,
+			);
 			toast.success("Chapter updated successfully");
 			toggleEdit();
 			router.refresh();
@@ -53,9 +57,7 @@ export const ChapterVideoForm = ({
 			<div className="font-medium flex items-center justify-between">
 				Chapter Video
 				<Button onClick={toggleEdit} variant="ghost">
-					{isEditing && (
-						<>Cancel</>
-					)}
+					{isEditing && <>Cancel</>}
 					{!isEditing && !initialData?.videoUrl && (
 						<>
 							<PlusCircle className="w-4 h-4 mr-2" />
@@ -70,19 +72,16 @@ export const ChapterVideoForm = ({
 					)}
 				</Button>
 			</div>
-			{!isEditing && (
-				!initialData.videoUrl ? (
+			{!isEditing &&
+				(!initialData.videoUrl ? (
 					<div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
 						<Video className="w-10 h-10 text-slate-500" />
 					</div>
 				) : (
 					<div className="relative aspect-video mt-2">
-						<MuxPlayer
-							playbackId={initialData?.muxData?.playbackId || ""}
-						/>
+						<MuxPlayer playbackId={initialData?.muxData?.playbackId || ""} />
 					</div>
-				)
-			)}
+				))}
 			{isEditing && (
 				<div>
 					<FileUpload
@@ -100,9 +99,10 @@ export const ChapterVideoForm = ({
 			)}
 			{initialData.videoUrl && !isEditing && (
 				<div className="text-xs text-muted-foreground mt-4">
-					Video can take a few minutes to process. Refresh the page to see the updated video.
+					Video can take a few minutes to process. Refresh the page to see the
+					updated video.
 				</div>
 			)}
 		</div>
 	);
-}
+};
