e29a80b53fba5be4ec4897f0dcfb1b05022ecff5
rick516 <k.murakami516@gmail.com>
2024-01-28 15:18:35 +0900

    add: query params automatic reflecion

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,24 @@
-import { db } from "@/lib/db"
-import { Categories } from "./_components/categories"
+import { SearchInput } from "@/components/search-input";
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
 
 const SearchPage = async () => {
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: "asc",
+		},
+	});
 
-  const categories = await db.category.findMany({
-    orderBy: {
-      name: "asc"
-    }
-  })
+	return (
+		<>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput/>
+      </div>
+			<div className="p-6">
+				<Categories items={categories} />
+			</div>
+		</>
+	);
+};
 
-
-  return ( 
-    <div className="p-6">
-      <Categories  items={categories}/>
-    </div>
-   );
-}
- 
 export default SearchPage;
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,55 +1,54 @@
 "use client";
 
-import { Layout, Compass, List, BarChart } from "lucide-react"
-import { SidebarItem } from "./sidebar-item";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
-  {
-    icon: Layout,
-    label: "Dashboard",
-    href: "/"
-  },
-  {
-    icon: Compass,
-    label: "Browse",
-    href: "/search"
-  },
-]
+	{
+		icon: Layout,
+		label: "Dashboard",
+		href: "/",
+	},
+	{
+		icon: Compass,
+		label: "Browse",
+		href: "/search",
+	},
+];
 
 const teacherRoutes = [
-  {
-    icon: List,
-    label: "Courses",
-    href: "/teacher/courses"
-  },
-  {
-    icon: BarChart,
-    label: "Analytics",
-    href: "/teacher/analytics"
-  },
-]
+	{
+		icon: List,
+		label: "Courses",
+		href: "/teacher/courses",
+	},
+	{
+		icon: BarChart,
+		label: "Analytics",
+		href: "/teacher/analytics",
+	},
+];
 
 export const SidebarRoutes = () => {
-  
-  const pathname = usePathname();
-  const isTeacherPage = pathname?.includes("/teacher");
-  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
-  
-  return (
-    <div className="flex flex-col w-full">
-      {routes.map((route) => {
-        return (
-          <SidebarItem
-            key={route.href}
-            label={route.label}
-            icon={route.icon}
-            href={route.href}
-          />
-        )
-      })}
-    </div>
-  );
-}
+	const pathname = usePathname();
+	const isTeacherPage = pathname?.includes("/teacher");
+	const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
+	return (
+		<div className="flex flex-col w-full">
+			{routes.map((route) => {
+				return (
+					<SidebarItem
+						key={route.href}
+						label={route.label}
+						icon={route.icon}
+						href={route.href}
+					/>
+				);
+			})}
+		</div>
+	);
+};
 
 export default SidebarRoutes;
