7c3f7e824594ba3fe5d09c174b57e4c8d2a7f3b6
rick516 <k.murakami516@gmail.com>
2024-01-30 01:43:13 +0900

    fix: change icon sm size

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -32,7 +32,8 @@ const iconVariants = cva(
       },
       size: {
         default: "w-10 h-10",
-        sm: "w-8 h-8",
+        sm: "w-4 h-4",
+        md: "w-8 h-8",
         lg: "w-12 h-12",
       },
       defaultVariants: {
