747062d641cbf43acfc2e7f2885c4a5f51ea4d6c
rick516 <k.murakami516@gmail.com>
2024-02-02 16:08:46 +0900

    add: chapter detail fetch

---
+++ actions/get-chapter.ts
@@ -0,0 +1,106 @@
+import { db } from "@/lib/db";
+import { Attachment, Chapter, MuxData } from "@prisma/client";
+
+interface getChapterProps {
+	userId: string;
+	courseId: string;
+	chapterId: string;
+}
+
+export const getChapter = async ({
+	userId,
+	courseId,
+	chapterId,
+}: getChapterProps) => {
+	try {
+		const purchase = await db.purchase.findUnique({
+			where: {
+				userId_courseId: {
+					userId,
+					courseId,
+				},
+			},
+		});
+
+		const course = await db.course.findUnique({
+			where: {
+				id: courseId,
+				isPublished: true,
+			},
+			select: {
+				price: true,
+			},
+		});
+
+		const chapter = await db.chapter.findUnique({
+			where: {
+				id: chapterId,
+				isPublished: true,
+			},
+		});
+
+		if (!chapter || !course) throw new Error("Chapter or course not found");
+
+		let muxData = null;
+		let attachments: Attachment[] = [];
+		let nextChapter: Chapter | null = null;
+
+		if (purchase) {
+			attachments = await db.attachment.findMany({
+				where: {
+					courseId,
+				},
+			});
+		}
+
+		if (chapter.isFree || purchase) {
+			muxData = await db.muxData.findFirst({
+				where: {
+					chapterId: chapterId,
+				},
+			});
+
+			nextChapter = await db.chapter.findFirst({
+				where: {
+					courseId: courseId,
+					isPublished: true,
+					position: {
+						gt: chapter.position,
+					},
+				},
+				orderBy: {
+					position: "asc",
+				},
+			});
+		}
+
+		const userProgress = await db.userProgress.findUnique({
+			where: {
+				userId_chapterId: {
+					userId: userId,
+					chapterId: chapterId,
+				},
+			},
+		});
+
+		return {
+			course,
+			chapter,
+			nextChapter,
+			attachments,
+			purchase,
+			userProgress,
+			muxData,
+		};
+	} catch (error) {
+		console.log("[GET_CHAPTER]", error);
+		return {
+			course: null,
+			chapter: null,
+			nextChapter: null,
+			attachments: [],
+			purchase: null,
+			muxData: null,
+		};
+	}
+};
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,10 +1,55 @@
-const ChapterIdPage = () => {
-  return ( 
-    <div>
-      chapter id
-    </div>
-   );
-}
- 
-export default ChapterIdPage;
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+
+const ChapterIdPage = async ({
+	params,
+}: {
+	params: {
+		courseId: string;
+		chapterId: string;
+	};
+}) => {
+	const { userId } = auth();
+	if (!userId) return redirect("/");
+
+	const {
+		course,
+		chapter,
+		nextChapter,
+		attachments,
+		userProgress,
+		purchase,
+		muxData,
+	} = await getChapter({
+		userId,
+		courseId: params.courseId,
+		chapterId: params.chapterId,
+	});
+
+	if (!chapter || !course) return redirect("/");
+
+	const isLocked = !chapter.isFree && !purchase;
+	const isCompletedOnEnd = !!purchase && !userProgress?.isCompleted;
+
+	return (
+		<div>
+			{userProgress?.isCompleted && (
+				<Banner
+					variant="success"
+					label="You have already completed this chapter."
+				/>
+			)}
+			{isLocked && (
+				<Banner
+					variant="warning"
+					label="you must purchase this course to access this chapter."
+				/>
+			)}
+		</div>
+	);
+};
+
+export default ChapterIdPage;
