f9f4eb11b6c56fa989c9011f00c95d044797be3a
rick516 <k.murakami516@gmail.com>
2024-02-17 23:05:58 +0900

    add: progress bar

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import axios from "axios";
 import { CheckCircle, XCircle } from "lucide-react";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 interface CourseProgressButtonProps {
 	chapterId: string;
@@ -16,18 +20,32 @@ export const CourseProgressButton = ({
 	nextChapterId,
 	isCompleted,
 }: CourseProgressButtonProps) => {
-
 	const Icon = isCompleted ? XCircle : CheckCircle;
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleProgress = async () => {
+		try {
+			setIsLoading(true);
+			// const response = await axios.post(`api/courses/${courseId}/chapters/${chapterId}/progress`);
+			toast.success("Chapter marked as completed");
+		} catch {
+			setIsLoading(true);
+			toast.error("Something went wrong");
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<Button
-			variant="default"
-			size="default"
-			disabled={isCompleted}
-			onClick={() => {}}
+			variant={!isCompleted ? "secondary" : "outline"}
+			size="sm"
+			disabled={isCompleted || isLoading}
+			onClick={handleProgress}
 		>
-			{isCompleted ? "UnCompleted" : "Mark as completed"}
-			<Icon />
+			{!isCompleted ? "Mark as completed" : "Completed"}
+			<Icon className="h-4 w-4 ml-2" />
 		</Button>
 	);
 };
