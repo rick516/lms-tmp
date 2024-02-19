b1b77bc8f499d058846f6e986a0941b8866960c8
rick516 <k.murakami516@gmail.com>
2024-02-19 17:54:58 +0900

    fix: progress api structured refctored

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -2,22 +2,20 @@ import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function PUT({
+export async function PUT(
 	req: Request,
-	{ params }: { params: { courseId: string; chapterId: string } },
-}) {
+	{ params }: { params: { courseId: string; chapterId: string } }
+) {
 	try {
 		const { userId } = auth();
 		if (!userId) return new NextResponse("Unauthorized", { status: 401 });
-
-		const { courseId, chapterId } = params;
-		const isCompleted = req.json().isCompleted;
+		const { isCompleted } = await req.json();
 
 		const userProgress = await db.userProgress.upsert({
 			where: {
 				userId_chapterId: {
 					userId,
-					chapterId,
+					chapterId: params.chapterId,
 				},
 			},
 			update: {
@@ -25,7 +23,7 @@ export async function PUT({
 			},
 			create: {
 				userId,
-				chapterId,
+				chapterId: params.chapterId,
 				isCompleted,
 			},
 		});
